6933996dc763e9307649f8e577c67b215bf7a922
ykosaka <ykosaka@student.42tokyo.jp>
2025-09-30 09:15:31 +0900

    Added Batch AES-GCM encode

---
+++ batch_aesgcm_enc.py
@@ -0,0 +1,41 @@
+import os, sys
+import datetime, time
+import my_common as my
+import myenclib
+
+def batch_aesgcm_enc(lst, key):
+    col_name = list(lst[0].keys())[0]
+    for i in range(len(lst)):
+        nonce, ciphertext = myenclib.aesgcm_enc(key, lst[i][col_name].encode('utf-8'))
+        encoded_data = myenclib.aesgcm_enc_b64(nonce, ciphertext)
+        lst[i]['encdata'] = encoded_data
+    return lst
+
+def wrapper(args):
+    if args and len(args) > 2 and len(args[1]) >= 16:
+        key = myenclib.b64_dec(args[2])
+    else:
+        key = myenclib.aesgcm_gen_key()
+    print(f"Secret Key (Base64): {myenclib.b64_enc(key)}")
+    if args and len(args) > 1:
+        lst = my.load_csv(args[1])
+    else:
+        lst = my.load_csv("names.txt")
+    return batch_aesgcm_enc(lst, key)
+
+if __name__ == "__main__":
+    start_at = datetime.datetime.now()
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    data_dir = BASE_DIR + "/data"
+    my.mkdir(data_dir, True)
+    date = my.get_datetime()
+    filepath = "cipher_" + date + ".csv"
+    filepath = data_dir + "/" + filepath
+
+    ret = wrapper(sys.argv)
+    # print(ret)
+    my.save_csv(ret, filepath)
+
+    finish_at = datetime.datetime.now()
+    print(f"Elapsed time: {finish_at - start_at}")
